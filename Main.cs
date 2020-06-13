f08149d4d85fe2689ab6b198f34cec036477937f
Aditya Keyal <adityak@nrifintech.com>
2020-06-13 12:50:40 +0530

    Initial Version

---
+++ Main.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Kbg.NppPluginNET.PluginInfrastructure;
+
+namespace Kbg.NppPluginNET
+{
+    class Main
+    {
+        internal const string PluginName = "MarkdownPlugin";
+        static string iniFilePath = null;
+        static bool someSetting = false;
+        static frmMyDlg frmMyDlg = null;
+        static int idMyDlg = -1;
+        static Bitmap tbBmp = Properties.Resources.star;
+        static Bitmap tbBmp_tbTab = Properties.Resources.star_bmp;
+        static Icon tbIcon = null;
+
+        public static void OnNotification(ScNotification notification)
+        {  
+            // This method is invoked whenever something is happening in notepad++
+            // use eg. as
+            // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
+            // { ... }
+            // or
+            //
+            // if (notification.Header.Code == (uint)SciMsg.SCNxxx)
+            // { ... }
+        }
+
+        internal static void CommandMenuInit()
+        {
+            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
+            iniFilePath = sbIniFilePath.ToString();
+            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
+            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
+            someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
+
+            PluginBase.SetCommand(0, "MyMenuCommand", myMenuFunction, new ShortcutKey(true, false, true, Keys.C));
+            //PluginBase.SetCommand(1, "MyDockableDialog", myDockableDialog); 
+            idMyDlg = 0;
+        }
+
+        internal static void SetToolBarIcon()
+        {
+            toolbarIcons tbIcons = new toolbarIcons();
+            tbIcons.hToolbarBmp = tbBmp.GetHbitmap();
+            IntPtr pTbIcons = Marshal.AllocHGlobal(Marshal.SizeOf(tbIcons));
+            Marshal.StructureToPtr(tbIcons, pTbIcons, false);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_ADDTOOLBARICON, PluginBase._funcItems.Items[idMyDlg]._cmdID, pTbIcons);
+            Marshal.FreeHGlobal(pTbIcons);
+        }
+
+        internal static void PluginCleanUp()
+        {
+            Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
+        }
+
+
+        internal static void myMenuFunction()
+        {
+
+
+            IntPtr currentScint = PluginBase.GetCurrentScintilla();
+            ScintillaGateway scintillaGateway = new ScintillaGateway(currentScint);
+                // Get selected text.
+            string selectedText = scintillaGateway.GetSelText();
+
+            var html = @"<table style=""margin: 1.2em 0px;padding: 0px; border-collapse: collapse; border-spacing: 0px; font: inherit; border: 0px;"">";
+
+            var lines = selectedText.Split('\n');
+            //var html = "<table style='box-sizing: inherit; font-family: arial, sans-serif; border-collapse: collapse; color: rgb(0, 0, 0); font-size: 15px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;'>";
+
+            var idx = 0;
+            foreach (var line in lines) {
+
+                if (idx % 2 == 0)
+                {
+                    html += @"<tr style=""border-width: 1px 0px 0px; border-right-style: initial; border-bottom-style: initial; border-left-style: initial; border-right-color: initial; border-bottom-color: initial; border-left-color: initial; border-image: initial; border-top-style: solid; border-top-color: rgb(204, 204, 204); background-color: white; margin: 0px; padding: 0px;background-color: rgb(248, 248, 248);"">";
+                }
+                else {
+                    html += @"<tr style=""border-width: 1px 0px 0px; border-right-style: initial; border-bottom-style: initial; border-left-style: initial; border-right-color: initial; border-bottom-color: initial; border-left-color: initial; border-image: initial; border-top-style: solid; border-top-color: rgb(204, 204, 204); background-color: white; margin: 0px; padding: 0px;"">";
+                }
+                var columns = line.Split('|');
+                foreach (var col in columns) {
+                    html += @"<td  style=""font-size: 1em; border: 1px solid rgb(204, 204, 204); margin: 0px; padding: 0.5em 1em; "">" + col + "</td>";
+                }
+                html += "</tr>";
+                idx++;
+            }
+            html += "</html>";
+                // if selectedText needs to be converted to markdown then do that
+                // for now convert to table markdown:
+
+
+
+                var myHtml = html;
+            var start = 99;
+            var startFragment = 133;
+            var endFragment = startFragment + html.Length;
+            var endHtml = endFragment  + 78;
+
+
+            var htmlFormat = "Version:0.9\r\n";
+            htmlFormat += "StartHTML: 000000102\r\n";
+            htmlFormat += "EndHTML:"+(endHtml+"").PadLeft(9,'0')+"\r\n";
+            htmlFormat += "StartFragment:000000134\r\n";
+            htmlFormat += "EndFragment:" + (endFragment + "").PadLeft(9, '0') + "\r\n";
+            htmlFormat += "<html><body><!--StartFragment-->"+ myHtml+ "<!--EndFragment--></body></html>";
+
+            var dataObject = new DataObject();
+            dataObject.SetData(DataFormats.Html, htmlFormat);
+            dataObject.SetData(DataFormats.Text, selectedText);
+            dataObject.SetData(DataFormats.Rtf, selectedText);
+            //dataObject.SetData(DataFormats.UnicodeText, selectedText);
+            Clipboard.SetDataObject(dataObject);
+
+
+        }
+
+        internal static void myDockableDialog()
+        {
+            if (frmMyDlg == null)
+            {
+                frmMyDlg = new frmMyDlg();
+
+                using (Bitmap newBmp = new Bitmap(16, 16))
+                {
+                    Graphics g = Graphics.FromImage(newBmp);
+                    ColorMap[] colorMap = new ColorMap[1];
+                    colorMap[0] = new ColorMap();
+                    colorMap[0].OldColor = Color.Fuchsia;
+                    colorMap[0].NewColor = Color.FromKnownColor(KnownColor.ButtonFace);
+                    ImageAttributes attr = new ImageAttributes();
+                    attr.SetRemapTable(colorMap);
+                    g.DrawImage(tbBmp_tbTab, new Rectangle(0, 0, 16, 16), 0, 0, 16, 16, GraphicsUnit.Pixel, attr);
+                    tbIcon = Icon.FromHandle(newBmp.GetHicon());
+                }
+
+                NppTbData _nppTbData = new NppTbData();
+                _nppTbData.hClient = frmMyDlg.Handle;
+                _nppTbData.pszName = "My dockable dialog";
+                _nppTbData.dlgID = idMyDlg;
+                _nppTbData.uMask = NppTbMsg.DWS_DF_CONT_RIGHT | NppTbMsg.DWS_ICONTAB | NppTbMsg.DWS_ICONBAR;
+                _nppTbData.hIconTab = (uint)tbIcon.Handle;
+                _nppTbData.pszModuleName = PluginName;
+                IntPtr _ptrNppTbData = Marshal.AllocHGlobal(Marshal.SizeOf(_nppTbData));
+                Marshal.StructureToPtr(_nppTbData, _ptrNppTbData, false);
+
+                Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_DMMREGASDCKDLG, 0, _ptrNppTbData);
+            }
+            else
+            {
+                Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_DMMSHOW, 0, frmMyDlg.Handle);
+            }
+        }
+    }
+}
