99d6381e6d36e91edc45156f6bab623ed1e0339a
Aditya Keyal <adityak@nrifintech.com>
2020-06-13 12:52:46 +0530

    Initial Version

---
+++ FlowChart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace MarkdownPlugin
+{
+    class FlowChart
+    {
+    }
+
+
+    public class Component {
+        string type;
+        string text;
+        string color = "#0f0fff";
+
+        public void Draw(){
+            Bitmap b = new Bitmap(200, 200);
+            var graphics = Graphics.FromImage(b);
+            graphics.Clear(Color.White);
+            graphics.FillRectangle(Brushes.Black, new Rectangle(0, 0, 100, 100));
+            
+            b.Save(@"d:\tmp\a.png", ImageFormat.Png);
+            graphics.Dispose();
+            b.Dispose();
+        }    
+    
+    }
+
+
+    
+}
